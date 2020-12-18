ed664d4b1839897500959267a573ae206364c626
SkyG0D <silvanosilvino@hotmail.com>
2020-12-18 02:44:50 -0200

    Gulp configured

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+
+const compileStyles = () => {
+    return gulp
+        .src('./src/scss/style.scss')
+        .pipe(sass({
+            noCache: true,
+            precision: 8,
+            unixNewlines: true,
+        }))
+        .pipe(prefix('last 3 version'))
+        .pipe(gulp.dest('./src/css'));
+}
+
+const watch = () => {
+    gulp.watch('./src/scss/**/*.scss', compileStyles)
+};
+
+gulp.task('sass', compileStyles);
+gulp.task('sass:watch', watch);
